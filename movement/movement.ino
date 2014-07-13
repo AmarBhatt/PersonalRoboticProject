1d7fba635aaa326159e34d0cb4dfcc975cffbdfe
Amar Bhatt <bhatt.amar.a@gmail.com>
2014-07-12 22:27:51 -0700

    Obstacle Avoidance (The Real Commit)
    My old code was deleted, so this is new code, still needs testing from
    my side.  However, nothing more than threshold numbers need to be
    changed.

--- movement/movement.ino
+++ movement/movement.ino
@@ -17,6 +17,20 @@ const int motorF1E = 3;
 const int motorR2E = 4;
 const int motorF2E = 5;
 
+const int trigPin = 7;
+const int echoPin = 8;
+
+const int TooClose = 2;
+const int OK = 10;
+const int Good = 15;
+
+const int halfSpeed = 128;
+const int threeQuartersSpeed = 188;
+const int fullSpeed = 250;
+
+int curDir = -1;
+int prevDir = -1;
+
 enum motorDirection {
   FORWARD,
   REVERSE,
@@ -37,25 +51,61 @@ void setup(){
   pinMode(motorF2A, OUTPUT);
   pinMode(motorF2B, OUTPUT);
   
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
   Serial.println("Start Motors");
 }
 
 void loop(){
-  //Serial.println("Reset Motors");
-  brake();
-  delay(2000);
-  //Serial.println("Drive Forward");
-  drive(255, FORWARD);
-  delay(2000);
-  //Serial.println("Pivot Left");
-  pivot(255, LEFT);
-  delay(4000);
-  //Serial.println("Drive Reverse");
-  drive(255, REVERSE);
-  delay(2000);
-  //Serial.println("Pivot RIGHT");
-  pivot(255, RIGHT);
-  delay(4000);  
+//  //Serial.println("Reset Motors");
+//  brake();
+//  delay(2000);
+//  //Serial.println("Drive Forward");
+//  drive(255, FORWARD);
+//  delay(2000);
+//  //Serial.println("Pivot Left");
+//  pivot(255, LEFT);
+//  delay(4000);
+//  //Serial.println("Drive Reverse");
+//  drive(255, REVERSE);
+//  delay(2000);
+//  //Serial.println("Pivot RIGHT");
+//  pivot(255, RIGHT);
+//  delay(4000);  
+
+  long inches = getDistanceFromObstacle();
+  
+  if (inches <= TooClose){
+    brake();
+    drive(halfSpeed, REVERSE);
+    delay(1000);
+    if(prevDir != -1){
+      if (curDir == -1){
+        curDir = (prevDir == RIGHT) ? LEFT : RIGHT;
+        pivot(fullSpeed, curDir);
+       delay(2000); 
+      }else{
+        pivot(fullSpeed, curDir);
+        delay(2000);
+      }     
+    }
+  }else if (inches <= OK){
+     if(prevDir == -1){
+       pivot(fullSpeed, RIGHT);
+       prevDir = RIGHT;
+       delay(2000);
+     }else {
+       pivot(fullSpeed, curDir);
+       prevDir = curDir;
+       delay(2000);
+     } 
+  }else {
+    drive(fullSpeed, FORWARD);
+    curDir = -1;
+    prevDir = -1;
+    delay(500);
+  }
 }
 
 void go(int motorPinA, int motorPinB, int motorEnable, int motorSpeed, int motorDirection){
@@ -109,3 +159,29 @@ void brake() {
   digitalWrite(motorF2A, LOW);
   digitalWrite(motorF2B, LOW);
 }
+
+long getDistanceFromObstacle() {
+  //Variables for inches and centimeter conversion
+  long duration;
+  //Ensure clean pulse
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, LOW);
+  
+  //Read Pulse Back
+  duration = pulseIn(echoPin, HIGH);
+  
+  //convert to inches
+  return microsecondsToInches(duration);
+}
+
+long microsecondsToInches(long microseconds){
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  return microseconds / 73.746 / 2;
+}//end microsecondsToInches
+  
